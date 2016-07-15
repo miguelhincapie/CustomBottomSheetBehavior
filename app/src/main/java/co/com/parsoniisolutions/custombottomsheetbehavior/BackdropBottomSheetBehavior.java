139dbe9df24e8cf29447938f00c1fce0b9a8f050
Paul-Marie Tetedoie <tpm444000@gmail.com>
2016-07-15 12:30:41 -0700

    Re-develop BackdropBottomSheetBehavior
    Re-develop BackdropBottomSheetBehavior to avoid using previous Y value and multiplier and use real time values instead. Make the behavior unaware of other behaviors. Add attributes for peekValue. We still need to work on the case someone want to change the peekHeight programmaticaly.
    This way it fix strange behavior where the ImageView attached to this behavior can be detached from the (GoogleStyle)BottomSheetBehavior depending on the strength of the sliding gesture. In addition it become reusable without any (GoogleStyle)BottomSheetBehavior !

--- app/src/main/java/co/com/parsoniisolutions/custombottomsheetbehavior/BackdropBottomSheetBehavior.java
+++ app/src/main/java/co/com/parsoniisolutions/custombottomsheetbehavior/BackdropBottomSheetBehavior.java
@@ -1,25 +1,26 @@
 package co.com.parsoniisolutions.custombottomsheetbehavior;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.widget.NestedScrollView;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewParent;
 
-/**
- *
- */
 public class BackdropBottomSheetBehavior<V extends View> extends CoordinatorLayout.Behavior<V> {
 
-    private float mImageHeight;
-    private float mYmultiplier;
-    private float mPreviousY;
-    private View mChild;
+    boolean mInit = false;
+    int mCollapsedY;
+    int mPeakHeight;
+    int mAnchorPointY;
+    int mCurrentChildY;
 
     public BackdropBottomSheetBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypedArray a = context.obtainStyledAttributes(attrs,R.styleable.BackdropBottomSheetBehavior_Params);
+        setPeekHeight(a.getDimensionPixelSize(R.styleable.BackdropBottomSheetBehavior_Params_behavior_backdrop_peekHeight, 0));
+        a.recycle();
     }
 
     @Override
@@ -28,64 +29,25 @@ public class BackdropBottomSheetBehavior<V extends View> extends CoordinatorLayo
     }
 
     @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, View child,
-                                          View dependency) {
-
-        if (mYmultiplier == 0) {
-            initValues(child, dependency);
-            return true;
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        if(!mInit){
+            init(child, dependency);
         }
-
-        float dVerticalScroll = dependency.getY() - mPreviousY;
-        mPreviousY = dependency.getY();
-
-        //going up
-        if (dVerticalScroll <= 0 && child.getY() <= 0) {
+        if((mCurrentChildY = (int) ((dependency.getY()-mAnchorPointY) * mCollapsedY / (mCollapsedY-mAnchorPointY))) <= 0)
             child.setY(0);
-            return true;
-        }
-
-        //going down
-        if (dVerticalScroll >= 0 && dependency.getY() <= mImageHeight)
-            return false;
-
-        child.setY( (int)(child.getY() + (dVerticalScroll * mYmultiplier) ) );
-
+        else
+            child.setY(mCurrentChildY);
         return true;
     }
 
+    private void init(@NonNull View child, @NonNull View dependency){
+        mCollapsedY = (int) (dependency.getY() - mPeakHeight);
+        mAnchorPointY = child.getHeight();
+        mCurrentChildY = (int) dependency.getY();
+        mInit = true;
+    }
 
-    private void initValues(View child, View dependency) {
-
-        BottomSheetBehaviorGoogleMapsLike bottomSheetBehavior = BottomSheetBehaviorGoogleMapsLike.from(dependency);
-        int peekHeight = bottomSheetBehavior.getPeekHeight();
-
-        ViewParent viewParent = dependency.getParent();
-        CoordinatorLayout coordinatorLayout = (CoordinatorLayout) viewParent;
-        float heightParent = coordinatorLayout.getHeight();
-
-        mImageHeight = child.getHeight();
-
-        float bottomSheetScroll_vertical_height = heightParent - mImageHeight - peekHeight;
-        mYmultiplier = (mImageHeight / bottomSheetScroll_vertical_height) + 1;
-
-        mPreviousY = dependency.getY();
-        child.setY((int) mPreviousY);
-
-
-        mChild = child;
-        bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehaviorGoogleMapsLike.BottomSheetCallback() {
-            @Override
-            public void onStateChanged(@NonNull View bottomSheet, @BottomSheetBehaviorGoogleMapsLike.State int newState) {
-                if (newState == BottomSheetBehaviorGoogleMapsLike.STATE_COLLAPSED ||
-                        newState == BottomSheetBehaviorGoogleMapsLike.STATE_HIDDEN)
-                    mChild.setY(bottomSheet.getY());
-            }
-
-            @Override
-            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-
-            }
-        });
+    public void setPeekHeight(int peakHeight) {
+        this.mPeakHeight = peakHeight;
     }
 }
