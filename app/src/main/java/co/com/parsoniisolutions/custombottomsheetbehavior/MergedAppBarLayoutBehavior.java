4a53b679e950341e4461c32715b7381212996d37
Paul-Marie Tetedoie <tpm444000@gmail.com>
2016-07-29 10:44:35 -0700

    Save MergedAppBarLayoutBehavior state

--- app/src/main/java/co/com/parsoniisolutions/custombottomsheetbehavior/MergedAppBarLayoutBehavior.java
+++ app/src/main/java/co/com/parsoniisolutions/custombottomsheetbehavior/MergedAppBarLayoutBehavior.java
@@ -7,6 +7,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Build;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.ColorRes;
 import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
@@ -17,6 +19,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewOutlineProvider;
@@ -30,6 +33,8 @@ import android.widget.TextView;
  *
  */
 public class MergedAppBarLayoutBehavior extends AppBarLayout.ScrollingViewBehavior {
+
+    private static final String TAG = MergedAppBarLayoutBehavior.class.getSimpleName();
 
     boolean mInit = false;
 
@@ -49,6 +54,7 @@ public class MergedAppBarLayoutBehavior extends AppBarLayout.ScrollingViewBehavi
     private View.OnClickListener mOnNavigationClickListener;
 
     private ValueAnimator mTitleAlphaValueAnimator;
+    private int mCurrentTitleAlpha = 0;
 
     public MergedAppBarLayoutBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -78,6 +84,7 @@ public class MergedAppBarLayoutBehavior extends AppBarLayout.ScrollingViewBehavi
         }else if(isDependencyYBetweenAnchorPointAndToolbar(child,dependency)){
 
             setToolbarVisible(true,child);
+            setFullBackGroundColor(android.R.color.transparent);
             setPartialBackGroundHeight(0);
 
         } else if(isDependencyYBelowToolbar(child, dependency) && ! isDependencyYReachTop(dependency)){
@@ -101,7 +108,25 @@ public class MergedAppBarLayoutBehavior extends AppBarLayout.ScrollingViewBehavi
         return false;
     }
 
+    @Override
+    public Parcelable onSaveInstanceState(CoordinatorLayout parent, View child) {
+        return new SavedState(super.onSaveInstanceState(parent, child),
+                mHidden,
+                mToolbarTitle,
+                mCurrentTitleAlpha);
+    }
+
+    @Override
+    public void onRestoreInstanceState(CoordinatorLayout parent, View child, Parcelable state) {
+        SavedState ss = (SavedState) state;
+        super.onRestoreInstanceState(parent, child, ss.getSuperState());
+        this.mHidden = ss.mHidden;
+        this.mToolbarTitle = ss.mToolbarTitle;
+        this.mCurrentTitleAlpha = ss.mTitleAlpha;
+    }
+
     private void init(@NonNull View child){
+
         AppBarLayout appBarLayout = (AppBarLayout) child;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             appBarLayout.setOutlineProvider(ViewOutlineProvider.BACKGROUND);
@@ -114,9 +139,14 @@ public class MergedAppBarLayoutBehavior extends AppBarLayout.ScrollingViewBehavi
         mTitleTextView = findTitleTextView(mToolbar);
         if (mTitleTextView == null)
             return;
-        mTitleTextView.setAlpha(0);
+
         mInitialY = child.getY();
-        child.setVisibility(View.INVISIBLE);
+
+        child.setVisibility(mHidden ? View.INVISIBLE : View.VISIBLE);
+        setStatusBarBackgroundVisible(!mHidden);
+        setFullBackGroundColor(mHidden ? android.R.color.transparent : R.color.colorPrimary);
+        mTitleTextView.setText(mToolbarTitle);
+        mTitleTextView.setAlpha(mCurrentTitleAlpha);
         mInit = true;
     }
 
@@ -219,7 +249,7 @@ public class MergedAppBarLayoutBehavior extends AppBarLayout.ScrollingViewBehavi
         if(mTitleAlphaValueAnimator == null || !mTitleAlphaValueAnimator.isRunning()){
             mToolbar.setTitle(mToolbarTitle);
             int startAlpha = visible ? 0 : 1;
-            int endAlpha = visible ? 1 : 0;
+            int endAlpha = mCurrentTitleAlpha = visible ? 1 : 0;
 
             mTitleAlphaValueAnimator = ValueAnimator.ofFloat(startAlpha,endAlpha);
             mTitleAlphaValueAnimator.setDuration(mContext.getResources().getInteger(android.R.integer.config_shortAnimTime));
@@ -250,7 +280,6 @@ public class MergedAppBarLayoutBehavior extends AppBarLayout.ScrollingViewBehavi
                 window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
                 window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
                 window.setStatusBarColor(ContextCompat.getColor(mContext,R.color.colorPrimaryDark));
-                mStatusBarBackground = new View(mContext);
 
             } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 if(mStatusBarBackground == null){
@@ -302,6 +331,48 @@ public class MergedAppBarLayoutBehavior extends AppBarLayout.ScrollingViewBehavi
 
     public void setAnchorPoint(float anchorPoint) {
         this.mAnchorPoint = anchorPoint;
+    }
+
+    protected static class SavedState extends View.BaseSavedState {
+
+        final boolean mHidden;
+        final String mToolbarTitle;
+        final int mTitleAlpha;
+
+        public SavedState(Parcel source) {
+            super(source);
+            mHidden = source.readByte() != 0;
+            mToolbarTitle = source.readString();
+            mTitleAlpha = source.readInt();
+        }
+
+        public SavedState(Parcelable superState, boolean hidden, String toolBarTitle, int titleAlpha) {
+            super(superState);
+            this.mHidden = hidden;
+            this.mToolbarTitle = toolBarTitle;
+            this.mTitleAlpha = titleAlpha;
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            super.writeToParcel(out, flags);
+            out.writeByte((byte) (mHidden ? 1 : 0));
+            out.writeString(mToolbarTitle);
+            out.writeInt(mTitleAlpha);
+        }
+
+        public static final Parcelable.Creator<SavedState> CREATOR =
+                new Parcelable.Creator<SavedState>() {
+                    @Override
+                    public SavedState createFromParcel(Parcel source) {
+                        return new SavedState(source);
+                    }
+
+                    @Override
+                    public SavedState[] newArray(int size) {
+                        return new SavedState[size];
+                    }
+                };
     }
 
     public static <V extends View> MergedAppBarLayoutBehavior from(V view) {
